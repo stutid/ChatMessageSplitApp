d3bcb46650b57a5d99ad21a06ec3b12d39dee24b
Stuti <stuti.dobhal@algoworks.com>
2019-03-31 03:49:12 +0530

    Initial Commit

---
+++ ChatMessageSplitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatMessageSplitApp
+//
+//  Created by Stuti on 31/03/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
